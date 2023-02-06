a87843a38dec47a119d4b8b21c7effe60dec6ba1
oscar-jesus <oscarperniamoreno@gmail.com>
2023-02-06 13:32:47 -0400

    code coverage, go tool cover y archivo coverage

--- testing/main.go
+++ testing/main.go
@@ -3,3 +3,10 @@ package main
 func Sum(a, b int) int {
 	return a + b
 }
+
+func GetMax(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
--- testing/main_test.go
+++ testing/main_test.go
@@ -10,6 +10,10 @@ func TestSum(t *testing.T) {
 			t.Errorf("Sum was incorrect, got %d expected %d", total, 11)
 		}
 	*/
+
+	// Code Coverage del 100% ya que la funcion "Sum()" solo tiene un
+	// statement if y en todos los casos retorna su valor exitosamente
+
 	tables := []struct {
 		a      int
 		b      int
@@ -24,6 +28,52 @@ func TestSum(t *testing.T) {
 		total := Sum(item.a, item.b)
 		if total != item.result {
 			t.Errorf("Sum was incorrect, got %d expected %d", total, item.result)
+		}
+	}
+}
+
+/*
+
+// Code Coverage del 75% ya que todos los casos que se presentan solo retornan
+// el primer statement pero nunca el segundo
+
+func TestGetMax(t *testing.T) {
+	tables := []struct {
+		a      int
+		b      int
+		result int
+	}{
+		{3, 2, 3},
+		{4, 2, 4},
+		{31, 30, 31},
+		{4, 3, 4},
+	}
+	for _, item := range tables {
+		max := GetMax(item.a, item.b)
+		if max != item.result {
+			t.Errorf("GetMax was incorrect, got %d expected %d", max, item.result)
+		}
+	}
+}
+*/
+
+// Code Coverage del 100% ya que en todos los casos la funcion "GetMax()"
+// logra retornar sus dos statements exitosamente
+func TestGetMax(t *testing.T) {
+	tables := []struct {
+		a      int
+		b      int
+		result int
+	}{
+		{3, 4, 4},
+		{4, 2, 4},
+		{31, 30, 31},
+		{4, 3, 4},
+	}
+	for _, item := range tables {
+		max := GetMax(item.a, item.b)
+		if max != item.result {
+			t.Errorf("GetMax was incorrect, got %d expected %d", max, item.result)
 		}
 	}
 }
